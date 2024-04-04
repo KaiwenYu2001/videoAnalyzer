ca08d26577f04b9cb5be65b5bc5197b25d49fe53
KaiwenYu2001 <kaiwenyu.kevin@gmail.com>
2024-04-04 14:06:33 +0800

    update

--- admin/admin/urls.py
+++ admin/admin/urls.py
@@ -1,4 +1,4 @@
-"""webServer URL Configuration
+"""admin URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.0/topics/http/urls/
